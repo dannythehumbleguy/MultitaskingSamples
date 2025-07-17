5c30a8fba64afdca94e069f29ac2e1620268f77d
Danil Zhiltsov <dannygam47@gmail.com>
2025-07-18 01:37:03 +0200

    PriorityTaskSchedulerV1 sample

---
+++ MultitaskingSamples/MultitaskingSamples.Cli/PriorityTaskSchedulerV1.cs
@@ -0,0 +1,47 @@
+﻿namespace MultitaskingSamples.Cli;
+
+public class PriorityTaskSchedulerV1(int maxParallelTasks)
+{
+    private readonly SortedDictionary<int, Queue<Func<Task>>> seq = new();
+    private readonly SemaphoreSlim semaphore = new(maxParallelTasks, maxParallelTasks);
+    
+    /// <summary>
+    /// Can't be invoked after RunAsync
+    /// </summary>
+    public void Enqueue(Func<Task> taskFactory, int priority)
+    {
+        if (!seq.ContainsKey(priority))
+            seq[priority] = new Queue<Func<Task>>();
+
+        seq[priority].Enqueue(taskFactory);
+    }
+    
+    public async Task RunAsync()
+    {
+        var tasks = new List<Task>();
+        
+        foreach (var key in seq.Keys)
+        {
+            var queue = seq[key];
+            foreach (var task in queue)
+            {
+                tasks.Add(WrapInSemaphore(task));
+            }
+            
+        }
+        await Task.WhenAll(tasks);
+    }
+
+    private async Task WrapInSemaphore(Func<Task> task)
+    {
+        try
+        {
+            await semaphore.WaitAsync();
+            await task.Invoke();
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+    }
+}
---
+++ MultitaskingSamples/MultitaskingSamples.Cli/Program.cs
@@ -0,0 +1,62 @@
+﻿using MultitaskingSamples.Cli;
+
+var scheduler = new PriorityTaskSchedulerV1(3);
+
+scheduler.Enqueue(async () =>
+{
+    Console.WriteLine("Low priority task");
+    await Task.Delay(2000);
+}, priority: 10);
+
+scheduler.Enqueue(async () =>
+{
+    Console.WriteLine("High priority task");
+    await Task.Delay(10000);
+    Console.WriteLine("High priority task ends");
+}, priority: 1);
+
+scheduler.Enqueue(async () =>
+{
+    Console.WriteLine("Medium priority task");
+    await Task.Delay(1500);
+}, priority: 5);
+
+scheduler.Enqueue(async () =>
+{
+    Console.WriteLine("Low priority task");
+    await Task.Delay(2000);
+}, priority: 12);
+
+scheduler.Enqueue(async () =>
+{
+    Console.WriteLine("High priority task");
+    await Task.Delay(10000);
+    Console.WriteLine("High priority task ends");
+}, priority: 3);
+
+scheduler.Enqueue(async () =>
+{
+    Console.WriteLine("Medium priority task");
+    await Task.Delay(1500);
+}, priority: 6);
+
+scheduler.Enqueue(async () =>
+{
+    Console.WriteLine("Low priority task");
+    await Task.Delay(2000);
+}, priority: 11);
+
+scheduler.Enqueue(async () =>
+{
+    Console.WriteLine("High priority task");
+    await Task.Delay(10000);
+    Console.WriteLine("High priority task ends");
+}, priority: 2);
+
+scheduler.Enqueue(async () =>
+{
+    Console.WriteLine("Medium priority task");
+    await Task.Delay(1500);
+}, priority: 4);
+
+await scheduler.RunAsync();
